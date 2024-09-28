ec9b9d0f23571664b7ba35ebfc7361026ff58c34
Elias Riveros <elias4845@gmail.com>
2024-09-28 00:07:46 -0400

    mensajes ahora se manejan con json

--- src/main/java/py/pol/una/sd/UDPServer.java
+++ src/main/java/py/pol/una/sd/UDPServer.java
@@ -1,5 +1,9 @@
 package py.pol.una.sd;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -65,64 +69,91 @@ public class UDPServer {
             while (running) {
                 // espera a recibir mensajes
                 // TODO: cerrar conexion cuando el cliente escriba chau, o con un timeout
-
+                try {
+                    synchronized (this) {
+                        this.wait();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
 
         public void handleMessages(DatagramPacket packet) {
             try {
+
                 String client_message = new String(packet.getData(), 0, packet.getLength());
+                Object obj = new JSONParser().parse(client_message);
+                JSONObject json_message = (JSONObject) obj;
                 System.out.println("Recibido de " + client_address + ":" + client_port + " - " + client_message);
 
                 // Responder al cliente
-                String response;
+                JSONObject response = new JSONObject();
                 byte[] send;
                 DatagramPacket send_packet;
 
-                if (user.isEmpty()) {
-                    user = client_message.trim();
-
+                if (json_message.get("tipo").equals("login")) {
+                    user = (String) json_message.get("mensaje");
+                    response.put("origen", "server");
                     if (users.containsKey(user)) {
-                        response = "Ese nombre de usuario ya existe";
+                        response.put("login", false);
+                        response.put("tipo", "error");
+                        response.put("mensaje", "usuario ya existe");
                     }
                     else {
                         users.put(user, this);
-                        response = "server: Hola " + user;
+                        response.put("login", true);
+                        response.put("tipo", "mensaje");
+                        response.put("mensaje", "Hola " + user);
                     }
                 }
                 // le vamos a enviar un mensaje a otro usuario
-                else {
-                    try {
-                        String receptor = client_message.substring(0, client_message.indexOf(":"));
+                else if (json_message.get("tipo").equals("mensaje")) {
+                    String receptor = (String) json_message.get("destino");
 
-                        if (!users.containsKey(receptor)) {
-                            response = "El usuario no se encuentra conectado al servidor";
-                        }
-                        else {
-                            users.get(receptor).sendMessage(user + ":" + client_message.substring(client_message.indexOf(":") + 1));
-                            response = "Mensaje enviado al usuario";
-                        }
-                    } catch (IndexOutOfBoundsException e) {
-                        response = "Formato de mensaje incorrecto";
+                    if (!users.containsKey(receptor)) {
+                        response.put("origen", "server");
+                        response.put("tipo", "error");
+                        response.put("mensaje", "El usuario no se encuentra conectado al servidor");
                     }
+                    else {
+                        users.get(receptor).sendMessage(user, (String) json_message.get("mensaje"));
+
+                        response.put("origen", "server");
+                        response.put("tipo", "info");
+                        response.put("mensaje", "Mensaje enviado al usuario");
+                    }
+
                 }
-                send = response.getBytes();
+                System.out.println(response);
+                send = response.toString().getBytes();
                 send_packet = new DatagramPacket(send, send.length, client_address, client_port);
                 socket.send(send_packet);
-            } catch (IOException e) {
+            } catch (IOException | ParseException e) {
                 throw new RuntimeException(e);
+            }
+            synchronized (this) {
+                notify();
             }
         }
 
-        public void sendMessage(String message) throws IOException {
+        public void sendMessage(String sender, String message) throws IOException {
+            JSONObject message_json = new JSONObject();
+
+            message_json.put("origen", sender);
+            message_json.put("tipo", "mensaje");
+            message_json.put("mensaje", message);
+
             byte[] send;
             DatagramPacket send_packet;
             System.out.println("Enviando mensaje a " + message);
-            send = message.getBytes();
+            send = message_json.toString().getBytes();
             send_packet = new DatagramPacket(send, send.length, client_address, client_port);
 
             socket.send(send_packet);
-
+            synchronized (this) {
+                notify();
+            }
         }
     }
 
